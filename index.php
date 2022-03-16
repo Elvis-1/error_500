53201f6c50a8b940546cd4d5846f43c997d1a371
Elvis <66243414+Elvis-1@users.noreply.github.com>
2022-03-16 05:29:40 +0100

    error_500

---
+++ index.php
@@ -0,0 +1,7 @@
+How to stop error 500 in projects cloned or downloaded from github
+
+<!-- composer install 
+mv .env.example .env 
+php artisan cache:clear 
+composer dump-autoload 
+php artisan key:generate -->
